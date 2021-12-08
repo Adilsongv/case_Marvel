f83dfbe3714a569350216ae9e30796523d0ee7dd
Thiago França <triggom@gmail.com>
2021-12-08 13:33:26 -0300

    correção de conflitos

--- src/app/register-address/register-address.component.ts
+++ src/app/register-address/register-address.component.ts
@@ -15,19 +15,11 @@ export class RegisterAddressComponent implements OnInit {
 
   constructor(private  cepsService: CepServiceService){}
 
-<<<<<<< HEAD
-  consultarCep(event:any, form: any){
-    this.cepsService.buscar(event.target.value).subscribe((dados) => this.populaForm(dados, form));
-  }
-
-  populaForm(dados: any, form: any){
-=======
   consultarCep(event:any, form:any){
     this.cepsService.buscar(event.target.value).subscribe((dados) => this.populaForm(dados, form));
   }
 
   populaForm(dados:any, form:any){
->>>>>>> 54e29703f400d1942cdea8ba64599b1d86df2635
     form.setvalue({
       cep: dados.cep,
       logradouro: dados.logradouro,
